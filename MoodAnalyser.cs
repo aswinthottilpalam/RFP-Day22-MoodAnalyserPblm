cacdfc065ecea2102aafeeaf10b544c262af7888
aswinthottilpalam <aswinp264@gmail.com>
2022-03-24 23:33:27 +0530

    ability to analyse mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserPgm
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        public MoodAnalyser()
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoodAnalyser"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// Analyses the mood and returns happy or mood
+        /// </summary>
+        /// <returns></returns>
+        public string AnalyseMood()
+        {
+            string mood;
+            if (message == "I am in a Happy mood")
+                mood = "Happy";
+            else if (message == "I am in a Sad mood")
+                mood = "Sad";
+            else
+                mood = null;
+            return mood;
+        }
+    }
+}
